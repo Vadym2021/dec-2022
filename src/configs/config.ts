b9820f064768e6a60621f5faabd81e2f30718622
aisclass <aisclass@ukr.net>
2023-06-13 21:01:12 +0300

    lesson 06 home

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,8 +1,14 @@
 import { config } from "dotenv";
+import * as process from "process";
 
 config();
 
 export const configs = {
   PORT: process.env.PORT,
   DB_URL: process.env.DB_URL,
+
+  SECRET_SALT:process.env.SECRET_SALT,
+
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
 };
---
+++ src/services/token.service.ts
@@ -0,0 +1,17 @@
+import * as jwt from 'jsonwebtoken';
+
+import { ITokenPair } from '../types/token.types';
+
+
+class TokenService {
+    public generateTokenPair(payload: Record<string, string | number>): ITokenPair {
+        const accessToken = jwt.sign(payload, "jwtAccess", {expiresIn: '15m'});
+        const refreshToken = jwt.sign(payload, "jwtRefresh", {expiresIn: '30d'});
+        return {
+            accessToken,
+            refreshToken,
+        };
+    }
+}
+
+export const tokenService = new TokenService();
