c15d5c739a1d321ce8e3dd7bd2b108ef7fe8621c
aisclass <aisclass@ukr.net>
2023-06-22 22:26:45 +0300

    lesson 08 home

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,4 +1,5 @@
 import { config } from "dotenv";
+import * as process from "process";
 
 config();
 
@@ -8,6 +9,11 @@ export const configs = {
 
   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+
+  JWT_FORGOT_SECRET: process.env.JWT_FORGOT_SECRET,
+  JWT_ACTIVATE_SECRET: process.env.JWT_ACTIVATE_SECRET,
+
+  FRONT_URL: process.env.FRONT_URL,
 
   SECRET_SALT: process.env.SECRET_SALT,
 
